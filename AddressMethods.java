503d08b0c54a3542fef3a69b8555f5423fca3ee3
naveeny061 <naveeny061@gmail.com>
2020-11-17 19:14:39 +0530

    Add: New class for add method

---
+++ AddressMethods.java
@@ -0,0 +1,12 @@
+package com.javapractice;
+
+import java.util.ArrayList;
+
+public class AddressMethods {
+	ArrayList<Address> addressBook = new ArrayList<Address>();
+
+	void Add() {
+		Address address = new Address();
+		addressBook.add(address);
+	}
+}
